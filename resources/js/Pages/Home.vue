5992b191b0bc503e19d1d203b7e9473ec68d3c56
I <91296724+i9191@users.noreply.github.com>
2025-09-09 18:17:14 +0700

    init

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="">
+        <h1>Arsip Surat Karangduren</h1>
+    </div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vuePlugin from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+        vuePlugin(),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+});
